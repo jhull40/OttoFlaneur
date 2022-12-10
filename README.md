a27c644b9009eeb33dcddb61bb6a7362ecace5f0
jhull40 <65205404+jhull40@users.noreply.github.com>
2022-12-09 21:34:36 -0500

    initial push
    does first few steps -
    1. grab random location
    2. get adjective
    3. create prompt for GPT
    4. pass prompt to GPT to create post

---
+++ README.md
@@ -0,0 +1,9 @@
+# Stick Reeves
+
+This is a pseudo-serious, but mostly for banter project that leverages new AI models to create an instagram bot.
+
+## Main Code
+Many thanks to [simplemaps](https://simplemaps.com/data/world-cities) for providing a huge list (43,000) cities of the world. I randomly sample from this (but sometimes making up a fake word), to generate the location for my post. 
+
+While I have a pre-defined set of a few words, I use [nltk](https://www.nltk.org/howto/wordnet.html) to generate some synonyms to change the words in the prompt. This prompt, which includes location and adjective selected at random, gets fed to GPT-3 to create an instagram post. 
+
---
+++ main.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from gibberish import Gibberish
+import random
+import nltk
+from nltk.corpus import wordnet
+import os
+import openai
+from secrets import OPENAI_API_KEY
+
+#os.environ["OPENAI_API_KEY"] = "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"
+
+
+
+def get_location():
+    cities = pd.read_csv('data/worldcities.csv')[['city', 'country']]
+    gib = Gibberish()
+
+    if random.random() > 0.85:
+        location = 0
+        location = gib.generate_word()
+    elif random.random() > 0.2:
+        vals = random.choice(cities.values)
+        location = f'{vals[0]}, {vals[1]}'
+    else:
+        location = random.choice(cities['city'])
+    
+    return location
+
+def get_adjective(words):
+    
+    synonyms = []  
+    for syn in wordnet.synsets(random.choice(words)):
+        for l in syn.lemmas():
+            synonyms.append(l.name())
+            
+    adjective = random.choice(synonyms)
+    return adjective
+
+def get_prompt(adjective, location):
+    prompt = f'Create an instagram post about my recent {adjective} trip to {location}'
+    return prompt
+
+def get_gpt3_post(prompt):
+    openai.api_key = OPENAI_API_KEY# os.getenv("OPENAI_API_KEY")
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=prompt,
+    temperature=0.9,
+    max_tokens=150,
+    top_p=1,
+    frequency_penalty=0.0,
+    presence_penalty=0.6,
+    stop=[" Human:", " AI:"]
+)
+    return response['choices'][0]['text']
+
+
+if __name__ == '__main__':
+    some_words = ['excellent', 'amazing', 'great', 'boring', 'fun', 'interesting']
+    location = get_location()
+    adjective = get_adjective(some_words)
+    prompt = get_prompt(adjective, location)
+    post = get_gpt3_post(prompt)
+    print(post)
